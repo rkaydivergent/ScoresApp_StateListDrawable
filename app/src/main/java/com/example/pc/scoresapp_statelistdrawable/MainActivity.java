9364409de8a5ffd6971718e325f4f28049388f2c
rkaydivergent <rimshakhanrk777@gmail.com>
2017-08-26 05:56:00 -0700

    Initial commit

---
+++ app/src/main/java/com/example/pc/scoresapp_statelistdrawable/MainActivity.java
@@ -0,0 +1,166 @@
+package com.example.pc.scoresapp_statelistdrawable;
+
+import android.graphics.drawable.StateListDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int mScore1;
+    private int mScore2;
+
+    //Member variable for the two score TextView
+    private TextView mScoreText1;
+    private TextView mScoreText2;
+
+    //Tag to be used as the key in OnSavedInstanceState
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static final String STATE_SCORE_2 = "Team 2 Score";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Find the TextViews by ID
+        mScoreText1 = (TextView) findViewById(R.id.score_1);
+        mScoreText2 = (TextView) findViewById(R.id.score_2);
+
+        //Restores the scores if there is savedInstanceState
+        if (savedInstanceState != null) {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            //Set the score text views
+            mScoreText1.setText(String.valueOf(mScore1));
+            mScoreText2.setText(String.valueOf(mScore2));
+        }
+
+
+    }
+
+    /**
+     * Creates the night mode menu option
+     *
+     * @param menu The menu in the action bar
+     * @return True to display the menu, false to hide it
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu from XML
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        //Change the label of the menu based on the state of the app
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else {
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    /**
+     * Method that handles menu item clicks
+     *
+     * @param item The item that was pressed
+     * @return returns true since the item click wa handled
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Check if the correct item was clicked
+        if (item.getItemId() == R.id.night_mode) {
+            //Get the night mode state of the app
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+
+            //Set the theme mode for the restarted activity
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+
+            //Recreate the activity for the theme change to take effect
+            recreate();
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Method that handles the onClick of both the decrement buttons
+     *
+     * @param view The button view that was clicked
+     */
+    public void decreaseScore(View view) {
+        //Get the ID of the button that was clicked
+        int viewID = view.getId();
+        switch (viewID) {
+            //If it was on Team 1
+            case R.id.decreaseTeam1:
+                //Decrement the score and update the TextView
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            //If it was Team 2
+            case R.id.decreaseTeam2:
+                //Decrement the score and update the TextView
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+
+        StateListDrawable stateListDrawable = new StateListDrawable();
+        stateListDrawable.addState(new int[]{android.R.attr.state_pressed, android.R.attr.state_enabled}, getResources().getDrawable(R.drawable.pressed_button_bg));
+        stateListDrawable.addState(new int[]{android.R.attr.state_focused}, getResources().getDrawable(R.drawable.pressed_button_bg));
+        stateListDrawable.addState(new int[]{}, getResources().getDrawable(R.drawable.button_background));
+        view.setBackgroundDrawable(stateListDrawable);
+    }
+
+    /**
+     * Method that handles the onClick of both the increment buttons
+     *
+     * @param view The button view that was clicked
+     */
+    public void increaseScore(View view) {
+        //Get the ID of the button that was clicked
+        int viewID = view.getId();
+        switch (viewID) {
+            //If it was on Team 1
+            case R.id.increaseTeam1:
+                //Increment the score and update the TextView
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            //If it was Team 2
+            case R.id.increaseTeam2:
+                //Increment the score and update the TextView
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+        StateListDrawable stateListDrawable = new StateListDrawable();
+        stateListDrawable.addState(new int[]{android.R.attr.state_pressed, android.R.attr.state_enabled}, getResources().getDrawable(R.drawable.pressed_button_bg));
+        stateListDrawable.addState(new int[]{android.R.attr.state_focused}, getResources().getDrawable(R.drawable.pressed_button_bg));
+        stateListDrawable.addState(new int[]{}, getResources().getDrawable(R.drawable.button_background));
+        view.setBackgroundDrawable(stateListDrawable);
+    }
+
+
+    /**
+     * Method that is called when the configuration changes, used to preserve the state of the app
+     *
+     * @param outState The bundle that will be passed in to the Activity when it is restored
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //Save the scores
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState);
+    }
+}
+
+
